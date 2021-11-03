3b784d5d7c264100688de4003845bae0946112cc
yuyaamano23 <nba.chako.nba@icloud.com>
2021-11-04 01:07:17 +0900

    deploy Heroku

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,16 @@
 <?php
 try {
-    $PDO = new PDO('mysql:dbname=message_board;host=localhost;charset=utf8', 'root', 'root');
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $user = $db['user'];
+    $password = $db['pass'];
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    );
+    $PDO = new PDO($dsn, $user, $password, $options);
 } catch (PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6fad7832bc70892419f330f7243b4038f2cb4292',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6fad7832bc70892419f330f7243b4038f2cb4292',
+            'dev_requirement' => false,
+        ),
+    ),
+);
